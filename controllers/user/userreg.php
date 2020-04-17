119adf0f97d29c9f379e8a0465ec312e31b26b03
IQper <you.us.be@bk.ru>
2020-04-17 17:01:03 +0500

    Update userreg.php
    Вынес бд в отдельный файл

--- controllers/user/userreg.php
+++ controllers/user/userreg.php
@@ -1,14 +1,13 @@
 <?php
-    //include("../../database/database.php");
+    include("../../database/database.php");
 
-    $bd = mysqli_connect("localhost", "root", "", "inteam");
     $email = $_POST["email"];
     $password = $_POST["password"];
     $date = date("Y-m-d H:i:s");
 
     if(!empty($email) && !empty($password)){
-        if($bd->query("SELECT * FROM members WHERE email = '$email'")->num_rows == 0){
-            $bd->query("INSERT INTO members VALUES(id, '$email', '$password', '$date')");
+        if($db->query("SELECT * FROM members WHERE email = '$email'")->num_rows == 0){
+            $db->query("INSERT INTO members VALUES(id, '$email', '$password', '$date')");
             http_response_code(200);
         }
         else
