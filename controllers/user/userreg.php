7ac84e041bebd11e927c8f1653451fda4725e162
IQper <you.us.be@bk.ru>
2020-04-17 16:58:29 +0500

    Update userreg.php
    Добавил время регистрации

--- controllers/user/userreg.php
+++ controllers/user/userreg.php
@@ -1,19 +1,22 @@
 <?php
     //include("../../database/database.php");
+
     $bd = mysqli_connect("localhost", "root", "", "inteam");
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $date = date("Y-m-d H:i:s");
 
-    if(!empty($_POST["email"]) && !empty($_POST["password"])){
-        $email = $_POST["email"];
-        $password = $_POST["password"];
-
+    if(!empty($email) && !empty($password)){
         if($bd->query("SELECT * FROM members WHERE email = '$email'")->num_rows == 0){
-            $bd->query("INSERT INTO members VALUES(id, '$email', '$password', date_reg)");
+            $bd->query("INSERT INTO members VALUES(id, '$email', '$password', '$date')");
             http_response_code(200);
         }
-        else{
-            http_response_code(400);
+        else
+        { 
+            http_response_code(400); 
         }
-    }else{
-        http_response_code(400); //BAD REQUEST
+    }
+    else{
+        http_response_code(400); 
     }
 ?>
