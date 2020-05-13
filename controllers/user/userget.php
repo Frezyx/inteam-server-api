24ed8aa9bc93a99d2ec15c10e30f70bc64df8219
IQper <you.us.be@bk.ru>
2020-05-13 15:18:11 +0500

    приличные изменения
    переделан формат приема данных с form-data на json

--- controllers/user/userget.php
+++ controllers/user/userget.php
@@ -1,15 +1,26 @@
 <?php
     include("../../database/database.php");
 
-    $email = $_POST["email"];
-    $password = $_POST["password"];
+    if($_SERVER["CONTENT_TYPE"] != 'application/json')
+    {
+        echo "wrong format (not json)";
+        return;
+    } 
+
+    $postData = file_get_contents('php://input');
+    $data = json_decode($postData, true);
+
+    $email = $data["email"];
+    $password = $data["password"];
 
     $date = date("Y-m-d H:i:s");
     $sql = "SELECT * FROM members WHERE email = '$email' AND password = '$password'";
     $result = mysqli_query($db, $sql);
     
-    if(!empty($email) && !empty($password)){
-        if($result->num_rows != 0){
+    if(!empty($email) && !empty($password))
+    {
+        if($result->num_rows != 0)
+        {
             $db->query(
                 "UPDATE members 
                 SET date_lastseen = '$date' 
@@ -20,11 +31,12 @@
         }
         else
         { 
-            http_response_code(400); 
+            echo "bad request (no user with such email & pass)";
         }
     }
-    else{
-        http_response_code(400); 
+    else
+    {
+        echo "bad request (null data)";
     }
     
 ?>
--- controllers/user/userreg.php
+++ controllers/user/userreg.php
@@ -1,21 +1,32 @@
 <?php
     include("../../database/database.php");
 
-    $email = $_POST["email"];
-    $password = $_POST["password"];
+    if($_SERVER["CONTENT_TYPE"] != 'application/json'){
+        echo "wrong format (not json)";
+        return;
+    } 
+
+    $postData = file_get_contents('php://input');
+    $data = json_decode($postData, true);
+
+    $email = $data["email"];
+    $password = $data["password"];
     $date = date("Y-m-d H:i:s");
 
-    if(!empty($email) && !empty($password)){
-        if($db->query("SELECT * FROM members WHERE email = '$email'")->num_rows == 0){
+    if(!empty($email) && !empty($password))
+    {
+        if($db->query("SELECT * FROM members WHERE email = '$email'")->num_rows == 0)
+        {
             $db->query("INSERT INTO members (email, password, date_reg, date_lastseen) VALUES('$email', '$password', '$date', '$date')");
             echo "registration succeed";
         }
         else
         { 
-            echo "bad request (bad data)";
+            echo "bad request (email is already exist)";
         }
     }
-    else{
+    else
+    {
         echo "bad request (null data)";
     }
 ?>
