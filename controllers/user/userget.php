651a62ff61a5d9f11a19042f706adf6863e4e7e9
IQper <you.us.be@bk.ru>
2020-05-22 17:58:12 +0500

    версия с хостинга

--- controllers/user/userget.php
+++ controllers/user/userget.php
@@ -1,42 +1,34 @@
 <?php
     include("../../database/database.php");
+    if(isset( $_POST["email"]) && isset($_POST["password"])){
+        if(!empty( $_POST["email"]) && !empty($_POST["password"])){
 
-    if($_SERVER["CONTENT_TYPE"] != 'application/json')
-    {
-        echo "wrong format (not json)";
-        return;
-    } 
-
-    $postData = file_get_contents('php://input');
-    $data = json_decode($postData, true);
-
-    $email = $data["email"];
-    $password = $data["password"];
-
-    $date = date("Y-m-d H:i:s");
-    $sql = "SELECT * FROM members WHERE email = '$email' AND password = '$password'";
-    $result = mysqli_query($db, $sql);
-    
-    if(!empty($email) && !empty($password))
-    {
-        if($result->num_rows != 0)
-        {
-            $db->query(
-                "UPDATE members 
-                SET date_lastseen = '$date' 
-                WHERE email = '$email' AND password = '$password'"
-            );
-            $result = mysqli_query($db, $sql);    
-            echo json_encode(mysqli_fetch_assoc($result));
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+        
+            $date = date("Y-m-d H:i:s");
+            $sql = "SELECT * FROM members WHERE email = '$email' AND password = '$password'";
+            $result = mysqli_query($db, $sql);
+            
+            if($result->num_rows != 0){
+                $db->query(
+                    "UPDATE members 
+                    SET date_lastseen = '$date' 
+                    WHERE email = '$email' AND password = '$password'"
+                );
+                $result = mysqli_query($db, $sql);    
+                echo json_encode(mysqli_fetch_assoc($result));
+            }
+            else
+            { 
+                echo "no user with such email and password";
+            }
         }
-        else
-        { 
-            echo "bad request (no user with such email & pass)";
+        else{
+            echo "password or email is empty";
         }
     }
-    else
-    {
-        echo "bad request (null data)";
+    else{
+        echo "password of email ain't set";
     }
-    
 ?>
--- controllers/user/userreg.php
+++ controllers/user/userreg.php
@@ -1,32 +1,25 @@
 <?php
     include("../../database/database.php");
+    if(isset( $_POST["email"]) && isset($_POST["password"])){
+        if(!empty( $_POST["email"]) && !empty($_POST["password"])){
 
-    if($_SERVER["CONTENT_TYPE"] != 'application/json'){
-        echo "wrong format (not json)";
-        return;
-    } 
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            $date = date("Y-m-d H:i:s");
 
-    $postData = file_get_contents('php://input');
-    $data = json_decode($postData, true);
-
-    $email = $data["email"];
-    $password = $data["password"];
-    $date = date("Y-m-d H:i:s");
-
-    if(!empty($email) && !empty($password))
-    {
-        if($db->query("SELECT * FROM members WHERE email = '$email'")->num_rows == 0)
-        {
-            $db->query("INSERT INTO members (email, password, date_reg, date_lastseen) VALUES('$email', '$password', '$date', '$date')");
-            echo "registration succeed";
-        }
-        else
-        { 
-            echo "bad request (email is already exist)";
+            if($db->query("SELECT * FROM members WHERE email = '$email'")->num_rows == 0){
+                $db->query("INSERT INTO members (email, password, date_reg, date_lastseen) VALUES('$email', '$password', '$date', '$date')");
+                echo "registration succeed";
+            }
+            else{
+                echo "such email is already exist";
+            }
+        }  
+        else{
+            echo "password or email is empty";
         }
     }
-    else
-    {
-        echo "bad request (null data)";
+    else{
+        echo "password of email ain't set";
     }
 ?>
