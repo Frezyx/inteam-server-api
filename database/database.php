a8c509ec3324d8a8f5d6b5b8335925efaa701e2f
unknown <youngfrezyx@gmail.com>
2020-05-23 03:16:57 +0300

    Чистка

--- database/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // $user = "id13494929_root";
-    // $password = "ENKLvoEtf%##3g2]";
-    $user = "root";
-    $password = "";
-
-    //$db = new PDO("mysql:host=localhost;dbname=inteam", $user, $password);
-    $db = mysqli_connect("localhost", $user, $password, "id13494929_inteam");
-?>  
